9465f61280798691a3bfac93d3bea8e819f65811
Abisha Accamma Vinod <77042240+abisha22@users.noreply.github.com>
2021-03-31 16:43:01 +0530

    python

---
+++ Programming Lab/17-01-2021/program 1.py
@@ -0,0 +1,8 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num=int(input("enter the number :"))
+enter the number :5
+>>> square=num*num
+>>> print(f"square of {num} is {square}")
+square of 5 is 25
+>>> 
